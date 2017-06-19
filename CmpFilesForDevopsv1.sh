f2eb1c1f40c5751afc123664118f07e84e04af79
ZackLaskar <Muzakkir@Muzakkirs-MacBook-Pro.local>
2017-06-19 20:20:10 +0530

    19June2017: Modified..

--- CmpFilesForDevopsv1.sh
+++ CmpFilesForDevopsv1.sh
@@ -3,10 +3,11 @@
 SourceDir="/Users/Muzakkir/PycharmProjects/Test"
 check=$SourceDir"/check.txt"   # check.txt will have entries of all the sql files in SourceDir
 
+findCksumCopy2temp() {
 
-findCksumCopy2temp () {
-	
-	# This function finds all sqls in the SourceDir and makes an entry of sql file names in a temp file called tmp.txt
+	# Usage :	
+	# finds all sqls in the SourceDir and makes an entry of sql file names in a temp file called tmp.txt
+	# loops thru file name in tmp and creates a new file with sql file name with its cksum in front of it, eg. example.sql ( 3282809881 )
 
 	if [[ $(find $1 -maxdepth 1 -type f -name '*.'$2) ]];then
 		basename `find $1 -maxdepth 1 -type f -name '*.'$2` > $1/tmp.txt
@@ -18,13 +19,23 @@ findCksumCopy2temp () {
 		echo " => No sqls found"
 		exit
 	fi
-	#rm $1/tmp.txt
 }
 
+rollback() {
+
+        if [[ -f "$1".rollback.sql ]];then
+                echo "\tExecuting rollback " $1.rollback.sql
+                echo ""
+        else
+                echo "     "$1".rollback.sql does not exist."
+                echo ""
+        fi
+}
 
 Compare() {
-	
-	# This function checks if the sqls in the directory are new then executes those sqls
+
+	# Usgae :	
+	# checks if the sqls in the directory are new then executes those sqls
 	# If old sqls there, then compares latest cksum with the old cksum, incase of any difference the rollbacks are run and then the modified sqls are exceuted. 
 
 	for f in "$@"
@@ -35,9 +46,9 @@ Compare() {
 			Checktxt_Cksum=`grep -w $f check.txt | awk '{print $3}'`
 
 			if [ "$Checktxt_Cksum" != "$Latest_Cksum" ];then
-				echo "   * "$f"'s cksum is different"
+				echo "   * "$f"'s cksum is different, looking for its rollback file.."
 				f=`echo $f | cut -d"." -f1`
-				[ -f "$f".rollback.sql  ] && echo "   * executing the "$f".rollback.sql" || echo "     "$f".rollback.sql does not exist"
+				rollback $f
 			fi
 		else	
 			# below variable stores all the new sqls filenames in it.
@@ -51,6 +62,7 @@ Compare() {
 	
 }
 
+
 findCksumCopy2temp $SourceDir sql
 
 if [[ ! -s $check ]]; then
@@ -61,9 +73,11 @@ else
 	echo " => Check.txt is not Empty."
         echo " => Comparing sql files in this directory with the filenames in check.txt.."
 	echo ""
-	Compare `cat tmp.txt`	
+	Compare `cat $SourceDir/tmp.txt`	
 fi
 
+rm $SourceDir/tmp.txt 
 mv $SourceDir/temp.txt $check
+
 
 
