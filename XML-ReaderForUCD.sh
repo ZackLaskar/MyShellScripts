b1e888ac248393e543130149fd24c368f7761c98
ZackLaskar <Muzakkir@Muzakkirs-MacBook-Pro.local>
2017-10-29 23:06:16 +0530

    29-Oct-2017

---
+++ XML-ReaderForUCD.sh
@@ -0,0 +1,43 @@
+#!/bin/ksh
+
+# script is for an urbancode deploy process template with reads XML and does stuff it need to do.
+ 
+
+IFS="," read -rA LocalQueueName <<< ${p:XMLReader/MQ.localQueues.localQname}
+IFS="," read -rA LocalQP <<< ${p:XMLReader/MQ.localQueues.localQP}
+IFS="," read -rA aliasQueueName <<< ${p:XMLReader/MQ.aliasQueues.alQname}
+IFS="," read -rA alQsomeProp <<< ${p:XMLReader/MQ.aliasQueues.alQsomeProp}
+
+echo ""
+echo 'Length of LocalQueueName :' ${#LocalQueueName[@]}
+echo ""
+
+if [[ ${#LocalQueueName[@]} == ${#LocalQP[@]} ]];then
+
+    for ((i=0;i<${#LocalQueueName[@]};i++));do
+        echo "${LocalQueueName[i]} ${LocalQP[i]}"
+    done
+
+else
+    echo 'Length of arrays not same.'
+    exit 1
+fi
+    
+
+
+echo ""
+echo 'Length of aliasQueueName :' ${#aliasQueueName[@]}
+echo ""
+if [[ ${#aliasQueueName[@]} == ${#alQsomeProp[@]} ]];then
+
+    for ((i=0;i<${#aliasQueueName[@]} ;i++));do
+        echo "${aliasQueueName[i]} ${alQsomeProp[i]}"
+    done
+
+else
+    echo 'Length of the arrays are not same.'
+   exit 1
+fi
+
+
+
