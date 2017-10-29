11c1a2de395d5caa960cf67f3715ebac9989f926
ZackLaskar <Muzakkir@Muzakkirs-MacBook-Pro.local>
2017-10-29 23:20:02 +0530

    29-Oct-2017

--- XML-ReaderForUCD.sh
+++ XML-ReaderForUCD.sh
@@ -1,6 +1,6 @@
 #!/bin/ksh
 
-# script is for an urbancode deploy process template with reads XML and does stuff it needs to do.
+# script is for an urbancode deploy process template which reads XML and does stuff it needs to do.
  
 
 IFS="," read -rA LocalQueueName <<< ${p:XMLReader/MQ.localQueues.localQname}
